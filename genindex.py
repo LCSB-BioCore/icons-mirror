5bfc8c411e5583a25e9edaf052f0baebe9a0bc87
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2023-03-02 09:34:49 +0100

    improve the site

--- genindex.py
+++ genindex.py
@@ -10,9 +10,11 @@ def get_codes():
 
 print("""<!doctype html>
 <html>
-<head><title>Twemoji listing</title></head>
+<head><title>Twemoji browsable listing</title></head>
 <body>
-All data on this page is generated from Twitter Twemoji, subject to the <a href="https://github.com/twitter/twemoji">corresponding license terms</a>.
+<h1> Twemoji browsable listing </h1>
+<p><strong>How-to</strong>: use Ctrl+F to find your favorite emoji by name.</p>
+<p>All data on this page is generated from Twitter Twemoji, subject to the <a href="https://github.com/twitter/twemoji">corresponding license terms</a>.</p>
 <table>
 """)
 
@@ -22,9 +24,9 @@ def safename(x):
     except:
         return 'UNKNOWN'
 
-for codes in get_codes():
+for codes in sorted(get_codes()):
     codestr='-'.join(codes)
-    print('<tr><td><img border="0" src="assets/72x72/'+codestr+'.png" /></td><td>'+codestr+'</td>')
+    print('<tr><td><div style="width: 72px; height: 72px; background-image: url(\'assets/72x72/'+codestr+'.png\')"></div></td><td>'+codestr+'</td>')
     print('<td>'+''.join(map(lambda x: chr(int(x, 16)), codes))+'</td>')
     print('<td>'+'→'.join(map(lambda x: safename(chr(int(x, 16))), codes))+'</td>')
     print('<td><a href="assets/svg/'+codestr+'.svg">svg</a></td><td><a href="assets/72x72/'+codestr+'.png">72x72 png</a></td></tr>')
